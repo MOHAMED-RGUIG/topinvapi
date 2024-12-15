be0aa441e54bfc2dc58f24ebba9ce9619756c1b3
MOHAMED-RGUIG <135667611+MOHAMED-RGUIG@users.noreply.github.com>
2024-12-15 11:46:18 +0100

    Update productsRoute.js

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -25,7 +25,38 @@ router.get('/getallproducts', async (req, res) => {
         return res.status(400).json({ message: 'Something went wrong', error: error.message });
     }
 });
-
+router.put('/updateproducts/:REFINV_0', async (req, res) => {
+    const { REFINV_0 } = req.params;
+    const { ETATINV } = req.body;
+  
+    try {
+      const pool = await poolPromise2;
+  
+      const result = await pool
+        .request()
+        .input('REFINV_0', sql.Int, REFINV_0)
+        .input('ETATINV', sql.NVarChar, ETATINV)
+        .query(`
+          UPDATE TCE.YLSTINV
+          SET ETATINV = @ETATINV
+          WHERE REFINV_0 = @REFINV_0
+        `);
+  
+      if (result.rowsAffected[0] > 0) {
+        res.json({
+          success: true,
+          message: 'Product updated successfully',
+          updatedProduct: { REFINV_0, ETATINV }, // Renvoie les nouvelles données
+        });
+      } else {
+        res.status(404).json({ success: false, message: 'Product not found' });
+      }
+    } catch (err) {
+      console.error('SQL error:', err.message);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+  
 
 router.get("/getallimgproducts",async(req,res)=>
 {
@@ -37,5 +68,7 @@ router.get("/getallimgproducts",async(req,res)=>
     }   
 }
 )
+
+
 module.exports = router;
 
