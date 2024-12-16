f35a62168b0f69715582e25602d952032247adba
MOHAMED-RGUIG <135667611+MOHAMED-RGUIG@users.noreply.github.com>
2024-12-16 14:00:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
     origin:["https://topinv.vercel.app"],
-    methods:["POST","GET"],
+    methods:["POST","GET","PUT"],
     credentials:true
     }));
 
